34bafda6b2ddb3077ffd2b8704a1a171ad4b785d
mheyrman <mcaro.heyrman@gmail.com>
2022-10-30 18:14:10 +0100

    comment

--- scripts/projection_system.cs
+++ scripts/projection_system.cs
@@ -1,10 +1,20 @@
-using System;
+using UnityEngine;
+using System.Collections;
 
-namespace projectionsystem {
-    class Projection {
-        static void Main(string[] args) {
-            
-            
-        }
+/*
+I (Matthias) will be working on this system a little bit as a refresher on OOP 
+and because I've already written out all of the maths
+*/
+
+public class ProjectionSystem : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
     }
 }
