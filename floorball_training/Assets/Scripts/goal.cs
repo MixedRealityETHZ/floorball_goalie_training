2f0c9315fbab1fe192cd5ed6b9275a2dac79e111
mheyrman <mcaro.heyrman@gmail.com>
2022-10-31 18:56:57 +0100

    played a bit

--- floorball_training/Assets/Scripts/goal.cs
+++ floorball_training/Assets/Scripts/goal.cs
@@ -1,21 +1,42 @@
-using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 // GameObject Goal class
 
-public class Goal : GameObject {
+public class Goal : MonoBehaviour {
 
-    public Goal() {
+    public static Vector3 center_position;
+    public static Quaternion orientation;
+    public static Vector3[] corner_positions;
+    public static bool was_seen = false;
 
+    public Goal(Vector3[] corner_positions, int[] corner_ids) {
+        /*
+         * Goal constructor that takes in the corner positions as seen
+         * from the Hololens and calculates the goal center position and angle
+         * based on the ids of the seen corners, and defines its corners based on that
+         */
     }
 
     // Use this for initialization
     void Start () {
-    
+        gameObject.GetComponent<Renderer>().material.color = new Color(0, 255, 0); // make the goal green
+
+        // hard coded
+        center_position = new Vector3(0, 1.15f / 2.0f, 0.0f);
+        orientation = Quaternion.identity;
+
+        // calculate corner positions based on center position:
+        corner_positions = new Vector3[4];
+        corner_positions[0] = center_position + new Vector3(1.6f / 2.0f, 1.15f / 2.0f, 0.6f / 2.0f);
+        corner_positions[1] = center_position + new Vector3(-1.6f / 2.0f, 1.15f / 2.0f, 0.6f / 2.0f);
+        corner_positions[2] = center_position + new Vector3(-1.6f / 2.0f, -1.15f / 2.0f, 0.6f / 2.0f);
+        corner_positions[3] = center_position + new Vector3(-1.6f / 2.0f, -1.15f / 2.0f, 0.6f / 2.0f);
     }
-    
+
     // Update is called once per frame
     void Update () {
-    
+          
     }
 }
--- floorball_training/Assets/Scripts/projection.cs
+++ floorball_training/Assets/Scripts/projection.cs
@@ -1,24 +1,26 @@
-using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 // GameObject Projection class
 // Projection is the projection of the 2D goal in front of the goalie
 
-public class Projection : GameObject {
+public class Projection : MonoBehaviour {
 
-    var center;
-    var theta;
-    var[] corners;
+    public Vector3 center;
+    public double angle;
+    public Vector3[] corners;
     
     /* constructor for the projection:
     array corner_positions: contains the positions on a flat plane of the corner positions relative to the center
     var center_position: center x,y,z position relative to Goal frame
     var orientation: represents the orientation (theta) of the (always vertical) projection of the goal 
     */
-    public Projection(var[] corner_positions, var center_position, var orientation) {
-        self.center = center_position;
-        self.theta = orientation;
-        self.corners = corner_positions;
+    // MAY NOT BE NECESSARY: NEED A WAY TO CHANGE SIZE TO ALWAYS BE 1 METER IN FRONT OF PLAYER
+    public Projection(Vector3[] corner_positions, Vector3 center_position, double orientation) {
+        center = center_position;
+        angle = orientation;
+        corners = corner_positions;
     }
 
     // Use this for initialization
